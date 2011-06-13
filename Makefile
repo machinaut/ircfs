08e3e5d91906d8530607a30c97be80517e8ff6b6
Alex Ray <ajray@us.ibm.com>
2011-06-13 09:23:02 -0500

    other changes to use ctl.go

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 include $(GOROOT)/src/Make.inc
 
 TARG=ircfs
-GOFILES=main.go
+GOFILES=main.go ctl.go
 
 include $(GOROOT)/src/Make.cmd
--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 )
 
 var n = 3
+var nick = "ajray"
+var ctl = NewCtlFile()
 
 type IrcFs struct {
     fuse.DefaultFileSystem
@@ -20,7 +22,17 @@ func (me *IrcFs) GetAttr(name string) (*os.FileInfo, fuse.Status) {
     log.Print("GetAttr " + name)
     switch name {
     case "file.txt":
-        return &os.FileInfo{Mode: fuse.S_IFREG | 0644, Size: int64(len(name))}, fuse.OK
+        return &os.FileInfo{Mode: fuse.S_IFREG | 0666, Size: int64(len(name))}, fuse.OK
+    case "ctl":
+        return &os.FileInfo{Mode: fuse.S_IFREG | 0666, Size: int64(len(name))}, fuse.OK
+    case "event":
+        return &os.FileInfo{Mode: fuse.S_IFREG | 0666, Size: int64(len(name))}, fuse.OK
+    case "nick":
+        return &os.FileInfo{Mode: fuse.S_IFREG | 0666, Size: int64(len(nick))}, fuse.OK
+    case "raw":
+        return &os.FileInfo{Mode: fuse.S_IFREG | 0666, Size: int64(len(name))}, fuse.OK
+    case "pong":
+        return &os.FileInfo{Mode: fuse.S_IFREG | 0666, Size: int64(len(name))}, fuse.OK
     case "":
         return &os.FileInfo{Mode: fuse.S_IFDIR | 0755}, fuse.OK
     }
@@ -30,8 +42,13 @@ func (me *IrcFs) GetAttr(name string) (*os.FileInfo, fuse.Status) {
 func (me *IrcFs) OpenDir(name string) (stream chan fuse.DirEntry, code fuse.Status) {
     log.Print("OpenDir " + name)
     if name == "" {
-        stream = make(chan fuse.DirEntry, 1) // , n + 5) // MUAHAHA NO BUFFER
+        stream = make(chan fuse.DirEntry, 6) // , n + 5) // MUAHAHA NO BUFFER
         stream <- fuse.DirEntry{Name: "file.txt", Mode: fuse.S_IFREG}
+        stream <- fuse.DirEntry{Name: "ctl", Mode: fuse.S_IFREG}
+        stream <- fuse.DirEntry{Name: "event", Mode: fuse.S_IFREG}
+        stream <- fuse.DirEntry{Name: "nick", Mode: fuse.S_IFREG}
+        stream <- fuse.DirEntry{Name: "raw", Mode: fuse.S_IFREG}
+        stream <- fuse.DirEntry{Name: "pong", Mode: fuse.S_IFREG}
         close(stream)
         return stream, fuse.OK
     }
@@ -40,13 +57,15 @@ func (me *IrcFs) OpenDir(name string) (stream chan fuse.DirEntry, code fuse.Stat
 
 func (me *IrcFs) Open(name string, flags uint32) (file fuse.File, code fuse.Status) {
     log.Print("Open " + name)
-    if name != "file.txt" {
-        return nil, fuse.ENOENT
+    switch name {
+    case "nick":
+        return fuse.NewReadOnlyFile([]byte(nick)), fuse.OK
+    case "file.txt":
+        return fuse.NewReadOnlyFile([]byte(name)), fuse.OK
+    case "ctl":
+        return ctl, fuse.OK
     }
-    if flags&fuse.O_ANYWRITE != 0 {
-        return nil, fuse.EPERM
-    }
-    return fuse.NewReadOnlyFile([]byte(name)), fuse.OK
+    return nil, fuse.ENOENT
 }
 
 func main() {
